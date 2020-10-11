42e7cbc1e55bd528ab838f2d32305f0628ac0816
Ali Berkin Şahin <aliberkinsahin@gmail.com>
2020-10-12 10:39:26 +1100

    header added

--- src/App.js
+++ src/App.js
@@ -58,50 +58,56 @@ function App() {
   };
 
   return (
-    <div className="container mx-auto shadow-lg sm:w-1/2 w-full p-6 bg-white rounded-md">
-      <form className="flex " onSubmit={handleItems}>
-        <input
-          className="rounded focus:outline-none py-2 px-4 border border-indigo-600 flex-1 mr-2"
-          type="text"
-          placeholder="Add a new item..."
-          value={input}
-          onChange={handleInput}
-        />
-        <button
-          type="submit"
-          className="border border-green-600 bg-green-500 hover:bg-green-700 text-white px-4 py-2 rounded font-bold focus:outline-none"
-        >
-          Add
-        </button>
-      </form>
-
-      <ul>
-        {items.map((_item, _index) => (
-          <li
-            className={`cursor-pointer font-semibold my-2 p-1 rounded hover:bg-gray-400 ${
-              _item.selected ? "bg-orange-600 text-white" : ""
-            }`}
-            key={_index}
-            onDoubleClick={() => removeItem(_index)}
-          >
-            {_item.text}
-          </li>
-        ))}
-      </ul>
-
-      {items.length > 0 && (
-        <>
-          <div className="border-t-2 border-gray-300 border-solid"></div>
+    <>
+    <h1 className="text-4xl font-semibold mb-2 ">
+      Randomizer
+    </h1>
+    <p className="text-xl mb-4 pb-3 border-b-2 border-gray-600">Enter your items and let computer gods decide...</p>
+      <div className="container mx-auto shadow-lg sm:w-1/2 w-full p-6 bg-white rounded-md">
+        <form className="flex " onSubmit={handleItems}>
+          <input
+            className="rounded focus:outline-none py-2 px-4 border border-indigo-600 flex-1 mr-2"
+            type="text"
+            placeholder="Add a new item..."
+            value={input}
+            onChange={handleInput}
+          />
           <button
-            className="border border-indigo-600 bg-indigo-500 hover:bg-indigo-700 text-white px-4 py-2 rounded font-bold focus:outline-none w-full mb-1 mt-2"
-            onClick={handleRadomize}
+            type="submit"
+            className="border border-green-600 bg-green-500 hover:bg-green-700 text-white px-4 py-2 rounded font-bold focus:outline-none"
           >
-            Randomize
+            Add
           </button>
-          <small>* Double click to remove an item.</small>
-        </>
-      )}
-    </div>
+        </form>
+
+        <ul>
+          {items.map((_item, _index) => (
+            <li
+              className={`cursor-pointer font-semibold my-2 p-1 rounded hover:bg-gray-400 ${
+                _item.selected ? "bg-orange-600 text-white" : ""
+              }`}
+              key={_index}
+              onDoubleClick={() => removeItem(_index)}
+            >
+              {_item.text}
+            </li>
+          ))}
+        </ul>
+
+        {items.length > 0 && (
+          <>
+            <div className="border-t-2 border-gray-300 border-solid"></div>
+            <button
+              className="border border-indigo-600 bg-indigo-500 hover:bg-indigo-700 text-white px-4 py-2 rounded font-bold focus:outline-none w-full mb-1 mt-2"
+              onClick={handleRadomize}
+            >
+              Randomize
+            </button>
+            <small>* Double click to remove an item.</small>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
